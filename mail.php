338138ddb788f942ada551fe5479c7d7205619f6
Zac McClung <zac.mcclung370@gmail.com>
2018-10-02 21:31:56 -0400

    saving

--- mail.php
+++ mail.php
@@ -2,6 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
+$to = "zac.mcclung370@gmail.com";s
 $recipient = "zac.mcclung370@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
